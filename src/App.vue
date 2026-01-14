0b7e5bfc0f3ed006cb9bec1e370e7ae662bb5077
Kevin-022C <keyfrans96@gmail.com>
2026-01-14 13:48:20 -0500

    Taller 22: Router Vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/hola">Hola Mundo</router-link> |
+    <router-link to="/preguntas">Preguntas</router-link>
+
   </nav>
   <router-view/>
 </template>
---
+++ src/views/PreguntaView.vue
@@ -0,0 +1,29 @@
+<template>
+   <div class="view-container">
+        <Pregunta/>
+   </div>
+</template>
+
+<script>
+import Pregunta from '@/components/Pregunta.vue';
+export default {
+   // name: 'Pregunta',
+    components: {
+        Pregunta
+    }
+}
+</script>
+
+<style scoped>
+.view-container {
+    display: flex;
+    min-height: 100vh;
+    padding: 20px;
+    justify-content: center;
+    align-items: center;
+}
+
+h2{
+    font-size: 28px;
+}
+</style>
